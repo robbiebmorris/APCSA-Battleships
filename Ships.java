e38981621634c14d0702ec7c0c54a54f32bc04ab
robbiebmorris <robbiebmorris@gmail.com>
2021-04-10 13:25:40 +0800

    added more functionality to ships class so that it now stores coordinates and orientation, as well as some getter methods for these values

--- Ships.java
+++ Ships.java
@@ -1,6 +1,9 @@
 public class Ships {
   int length;
   Node symbol;
+  int row;
+  int col;
+  boolean orientation;
 
   public Node getSymbol() {
     return this.symbol;
@@ -8,6 +11,23 @@ public class Ships {
 
   public int getLength() {
     return this.length;
+  }
+
+  public void setCoordinates(int newRow, int newCol, boolean orientation) {
+    this.row = newRow;
+    this.col = newCol;
+  }
+
+  public int getRow() {
+    return this.row;
+  }
+
+  public int getCol() {
+    return this.col;
+  }
+
+  public boolean getOrientation() {
+    return this.orientation;
   }
 
 }
