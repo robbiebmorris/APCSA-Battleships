1e1927f75714b6211ae23b3fce7e501c50307712
robbiebmorris <robbiebmorris@gmail.com>
2021-04-11 12:14:07 +0800

    added comments to Node class

--- Node.java
+++ Node.java
@@ -1,3 +1,5 @@
+//standard enumerate class to simplify the board class
+
 public enum Node {
   A('A'), B('B'), C('C'), D('D'), EMPTY('-'), BLANK('O'), HIT('X');
 
@@ -7,6 +9,7 @@ public enum Node {
     this.node = newNode;
   }
 
+  // getter method
   public char getNode() {
     return this.node;
   }
